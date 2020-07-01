8f5ff2bf9832939ac68463a2df852522e706bdc9
Joao Sousa <joao.rsousa23@gmail.com>
2020-07-01 23:22:53 +0930

    home styled, logo and background added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
 
+import {
+  Container, 
+  LogoImage, 
+  Main, 
+  Title, 
+  Description
+} from './styles';
 
 const Home = () => {
   return (
-    <Container>
-      <Text>Hello World</Text>
+    <Container 
+    source={require('../../assets/home-background.png')}
+    imageStyle={{ width: 274, height: 368}}
+    >
+      <Main>
+        <LogoImage source={require('../../assets/logo.png')}/>
+        <Title>Your place to collect recyclable items</Title>
+        <Description>We help people to find collection points efficiently</Description>
+      </Main>
+      
     </Container>
   )
 }
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -1,5 +1,40 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-  
+
+export const Container = styled.ImageBackground`
+  flex: 1;
+  padding: 32px;
+  background-color: #f0f0f5;
+`;
+
+export const Main = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
+
+export const LogoImage = styled.Image`
+
+`;
+
+export const Footer = styled.View`
+
+`;
+
+
+export const Title = styled.Text`
+  color: #322153;
+  font-size: 32px;
+  font-family: Ubuntu_700Bold;
+  max-width: 260px;
+  margin-top: 64px;
+`;
+
+
+export const Description = styled.Text`
+  color: #6C6C80;
+  font-size: 16px;
+  margin-top: 16px;
+  font-family: Roboto_400Regular;
+  max-width: 260px;
+  line-height: 24px;
 `;
